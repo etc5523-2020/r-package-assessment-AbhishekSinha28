71d64a215473f0c8c097436a8872a1ca08494b25
Abhishek Sinha <asin0080@student.monash.edu>
2020-10-22 22:58:54 +1100

    documenting aus_covid_data.

--- R/aus_covid_data.R
+++ R/aus_covid_data.R
@@ -1,6 +1,6 @@
 #' Daily COVID19 cases and deaths in Australia by state
 #'
-#' This data set contains COVID19 related data from states and territories in Australia from the 22nd of January to the 28th of September 2020. There are 7 variables in the dataset including province representing states and territories in Australia, type of cases including confirmed, death and recovered.
+#' This data set contains COVID19 related data from states and territories in Australia from the 22nd of January to the most recent date. There are 7 variables in the dataset including province representing states and territories in Australia, type of cases including confirmed, death and recovered.
 #'
 #' @source 
 #' The raw data pulled from the Johns Hopkins University Center for Systems Science and Engineering (JHU CCSE) Coronavirus repository.
